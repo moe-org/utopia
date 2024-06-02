d83ba0b70fcfbd255dae9dbd9902c11325d3ba8f
mingmoe <me@kawayi.moe>
2024-06-02 12:24:35 +0800

    update

---
+++ src/Utopia.Core/Exceptions/NoMatchGuuidException.cs
@@ -0,0 +1,21 @@
+// This file is a part of the project Utopia(Or is a part of its subproject).
+// Copyright 2020-2023 mingmoe(http://kawayi.moe)
+// The file was licensed under the AGPL 3.0-or-later license
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utopia.Core.Exceptions;
+public sealed class NoMatchGuuidException : Exception
+{
+    public Guuid ExpectGuuid { get; init; }
+
+    public NoMatchGuuidException(Guuid guuid, string msg) : base($"No match guuid:expect {guuid} but:{msg}")
+    {
+        ExpectGuuid = guuid;
+    }
+
+}
