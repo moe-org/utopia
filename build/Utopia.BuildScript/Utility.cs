b8a1b8fd7018243bc00fcec635049025550b8567
mingmoe <me@kawayi.moe>
2024-06-02 12:35:59 +0800

    update

--- build/Utopia.BuildScript/Utility.cs
+++ build/Utopia.BuildScript/Utility.cs
@@ -14,41 +14,48 @@ public static class Utility
 {
     public static string? FindProgram(params string[] featureNames)
     {
-        var env = Environment.GetEnvironmentVariable("PATH");
-        string[] path;
+        try
+        {
+            var env = Environment.GetEnvironmentVariable("PATH");
+            string[] path;
 
-        if (env is null)
+            if (env is null)
+            {
+                return null;
+            }
+
+            if (OperatingSystem.IsWindows())
+            {
+                path = env.Split(';');
+            }
+            else
+            {
+                path = env.Split(":");
+            }
+
+            foreach (var p in path)
+            {
+                foreach (var f in Directory.GetFiles(Path.GetFullPath(p)))
+                {
+                    bool match = true;
+                    foreach (var feature in featureNames)
+                    {
+                        if (!f.Contains(feature))
+                        {
+                            match = false;
+                        }
+                    }
+
+                    if (match)
+                        return Path.GetFullPath(f);
+                }
+            }
+
+            return null;
+        }
+        catch (Exception)
         {
             return null;
         }
-
-        if (OperatingSystem.IsWindows())
-        {
-            path = env.Split(';');
-        }
-        else
-        {
-            path = env.Split(":");
-        }
-
-        foreach (var p in path)
-        {
-            foreach (var f in Directory.GetFiles(p))
-            {
-                bool match = true;
-                foreach (var feature in featureNames)
-                {
-                    if (!f.Contains(feature))
-                    {
-                        match = false;
-                    }
-                }
-
-                if (match)
-                    return Path.GetFullPath(Path.Join(p, f));
-            }
-        }
-
-        return null;
     }
 }
